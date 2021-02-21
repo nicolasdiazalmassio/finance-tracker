2be50fe1c040fa9bf4bce4ed242d18e97a94f7bc
nicolas diaz almassio <nicolas.diaz.almassio@gmail.com>
2021-02-21 17:18:44 -0300

    Added myfriends page and search method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -27,5 +30,9 @@ class User < ApplicationRecord
     else
     "Anonymus"
     end
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
   end
 end
