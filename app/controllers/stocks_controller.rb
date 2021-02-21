4a63af94806703f2a1c625a817e030b45eb2a279
nicolas diaz almassio <nicolas.diaz.almassio@gmail.com>
2021-02-21 09:53:09 -0300

    Added user_stock relation and creation

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -11,11 +11,13 @@ class StocksController < ApplicationController
                 respond_to do |format|
                     flash.now[:alert] = "Please enter a valid symbol to search"
                     format.js { render partial: 'users/result' }
+                end
             end
         else
             respond_to do |format|
                 flash.now[:alert] = "Please enter a symbol to search"
                 format.js { render partial: 'users/result' }
+            end
         end    
     end
 
